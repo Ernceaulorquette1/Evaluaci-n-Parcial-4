116faf456bacc454b2e72d9ca745eebf58674907
Ernceaulorquette1 <er.lorquette@duocuc.cl>
2025-06-25 21:58:26 -0400

    Add files via upload

---
+++ uu.py
@@ -0,0 +1,63 @@
+
+
+#Teatro_CafeConLeche (): 
+#def mi_funcion():
+#def Cats ():
+
+def Comprar_entrada():
+    nombre= input("ingresar nombre de comprador")
+    return  
+Comprar_entrada = {}
+   
+     
+
+
+while True:
+    print("TOTEM AUTOATENCIÓN CAFECONLECHE")
+    print("1.- Comprar entrada a Cats.")
+    print("2.- Cambio de función.")
+    print("3.- Mostrar stock de funciones.")
+    print("4.- Salir.")
+
+    opcion=0
+
+    try:
+        opcion = int(input("Ingrese opción: "))
+    except ValueError:
+        print("La opción debe ser un número")
+    else:
+        if  opcion==1 :
+            nombre= input("ingresar nombre de comprador")
+
+            if nombre in Comprar_entrada :
+                print(" el nombre ya existe")
+                break     
+            else:
+
+                Función =[]
+                entrada= [1,180]
+                entrada += 180
+                for i in Función [1,180] :
+                    if Función == 1 and entrada <150 :
+                       
+                        print( " entrada es dias Cats Día Viernes")
+                    else : 
+                        print( " entrada esCats Día Sábado")
+        elif opcion==2 :
+            while True:
+                if nombre in Comprar_entrada :
+                    print(" el nombre ya existe")
+                    break  
+
+        elif opcion==3 :
+            print (f" el total entreda es : {entrada-180})
+                   
+        else: 
+        
+            print ("Programa terminado...")
+        
+
+
+           
+
+            
